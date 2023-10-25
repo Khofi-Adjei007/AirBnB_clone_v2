5ce884cf8748c32ff7c6946eacd5a8c92144ab23
Khofi-Adjei007 <khofiadjei@gmail.com>
2023-10-25 15:33:06 -0700

    Test II

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,17 +1,37 @@
+#!/usr/bin/python3
 from flask import Flask
 
-app = Flask(__name__)
+# Create a Flask web application instance
+app = Flask(__name)
+
+# Define a route for the root URL ('/') that displays a greeting
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Display a greeting message.
+
+    Returns:
+        str: A greeting message.
+    """
     return "Hello HBNB!"
+
+# Define a route for the '/hbnb' URL that displays 'HBNB'
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
+def display_hbnb():
+    """
+    Display 'HBNB'.
+
+    Returns:
+        str: The string 'HBNB'.
+    """
     return "HBNB"
 
 
-if __name__ == '__main__':
+# Run the Flask application if this script is executed
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
